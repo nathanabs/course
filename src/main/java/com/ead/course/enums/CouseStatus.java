d8c254e8649a81ea03b61e1be27b9a116127b7a0
Nathan Aguiar <nathanaguiarbs@gmail.com>
2021-12-08 19:14:58 -0300

    Criação de classes de modelos e serviços

---
+++ src/main/java/com/ead/course/enums/CouseStatus.java
@@ -0,0 +1,6 @@
+package com.ead.course.enums;
+
+public enum CouseStatus {
+    IN_PROGRESS,
+    CONCLUDED
+}
---
+++ src/main/java/com/ead/course/model/CourseModel.java
@@ -0,0 +1,52 @@
+package com.ead.course.model;
+
+import com.ead.course.enums.CourseLevel;
+import com.ead.course.enums.CouseStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_COURSES")
+public class CourseModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID couseId;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column
+    private String imageUrl;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime lastUpdateDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CouseStatus couseStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CourseLevel courseLevel;
+
+    @Column(nullable = false)
+    private UUID userInstructor;
+}
