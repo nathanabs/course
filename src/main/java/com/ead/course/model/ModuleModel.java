98e09e696a0f66f95733aea614c377bee6e33525
Nathan Aguiar Barbosa Silva <nathanaguiarbs@gmail.com>
2023-11-25 15:10:18 -0300

    utilizando o Padrão ISO 8601 UTC para datas

--- src/main/java/com/ead/course/model/ModuleModel.java
+++ src/main/java/com/ead/course/model/ModuleModel.java
@@ -30,7 +30,7 @@ public class ModuleModel implements Serializable {
     private String description;
 
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime creationDate;
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
